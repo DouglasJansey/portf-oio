457c310659099381799bb5cef328f87c9dd94138
douglas Jansey <douglasJansey>
2024-06-17 14:05:55 -0300

    add: input validate

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -10,35 +10,49 @@ interface formProps {
     subject: string;
     mensage: string;
 }
+type FormKey = keyof formProps
+
+const initialState = {
+    name: '',
+    email: '',
+    subject: '',
+    mensage: ''
+}
 
 export default function Contact() {
     const [isLoading, setIsloading] = useState(false)
+    const [checkInput, setCheckInput] = useState<string[]>([])
     const [status, setStatus] = useState(0)
-    const [formData, setFormData] = useState<formProps>({
-        name: '',
-        email: '',
-        subject: '',
-        mensage: ''
-    })
+    const [formData, setFormData] = useState<formProps>(initialState)
+    const CheckInput = (inputValue: EventTarget) => {
+        const inputKeys = Object.values(inputValue)
+        const inputErrors: string[] = []
+        const values = inputKeys.map(item => (typeof item === 'object') && !item.value && item)
+        values.forEach((item) => Object.keys(formData).includes(item.name) && inputErrors.push(item.name))
+        setCheckInput(inputErrors)
+    }
+    const inputError = (inputName: string) => {
+        const error = checkInput.includes(inputName)
+        return error ? '2px solid red' : ''
+    }
     const formHandler = ({ target }: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = target
 
         setFormData({ ...formData, [name]: value })
     }
+    console.log(checkInput)
     const ClearMensage = () => {
         setTimeout(() => {
             setStatus(0)
-            setFormData({
-                name: '',
-                email: '',
-                subject: '',
-                mensage: ''
-            })
+            setFormData(initialState)
         }, 5000)
 
     }
     const handleSubmit = (ev: FormEvent) => {
         ev.preventDefault()
+        const inputValues = ev.target
+        CheckInput(inputValues)
+
         try {
             setIsloading(true)
             fetch(`/api/send`, {
@@ -70,19 +84,19 @@ export default function Contact() {
                 <form className={style.containerForm} onSubmit={(ev) => handleSubmit(ev)}>
                     <h3 style={{ color: '#fff' }}>ENTRAR EM CONTATO</h3>
                     <label className={style.containerLabel}>
-                        <input className={style.inputText} type='text' name='name' placeholder='Seu nome'
+                        <input className={style.inputText} type='text' style={{ border: inputError('name') }} name='name' placeholder='Seu nome'
                             onChange={(e) => formHandler(e)} value={formData.name} />
                     </label>
                     <label className={style.containerLabel}>
-                        <input className={style.inputText} type='email' name='email' placeholder='Endereço de email válido'
+                        <input className={style.inputText} type='email' style={{ border: inputError('email') }} name='email' placeholder='Endereço de email válido'
                             onChange={(e) => formHandler(e)} value={formData.email} />
                     </label>
                     <label className={style.containerLabel}>
-                        <input className={style.inputText} type='text' name='subject' placeholder='Assunto'
+                        <input className={style.inputText} type='text' style={{ border: inputError('subject') }} name='subject' placeholder='Assunto'
                             onChange={(e) => formHandler(e)} value={formData.subject} />
                     </label>
                     <label className={style.containerLabel}>
-                        <textarea className={style.textArea} name='mensage' placeholder='Sua mensagem'
+                        <textarea className={style.textArea} style={{ border: inputError('mensage') }} name='mensage' placeholder='Sua mensagem'
                             onChange={(e) => formHandler(e)} value={formData.mensage} />
                     </label>
                     <Button type='submit' style={{ width: '250px', height: '45px' }}>
