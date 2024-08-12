b88359ade20165f7d20403eb30bdad378491dfbb
Douglas <douglas.jansey@gmail.com>
2024-08-12 18:49:29 -0300

    update about me

--- src/components/aboutme/about/index.tsx
+++ src/components/aboutme/about/index.tsx
@@ -12,6 +12,7 @@ import style from "./Aboutme.module.sass";
 export default function AboutMe() {
   const [activeLanguage, setActiveLanguage] = useState(false)
   const [activeExperience, setActiveExperience] = useState(false)
+  const [seeMore, setSeeMore] = useState(false)
   const profileDesc = profile.desc
   const refLang = useRef<HTMLDivElement>(null);
 
@@ -20,50 +21,61 @@ export default function AboutMe() {
     handleDivHeight()
     setActiveLanguage(scrollY > 0);
   }
-  const handleDivHeight = () => {
-    const rect = refLang.current?.getBoundingClientRect();
-    
-    if(rect){
-      if (rect.width && rect.height) {
-        // A div está visível na tela, atualiza a altura
-        const divHeight = refLang.current!
-        scrollY > divHeight?.clientHeight && divHeight?.clientHeight > 0 ? setActiveExperience(true) : setActiveExperience(false)       
-        
-      }
-      
+  const handleSeeMore = (): {} => {
+    const seeMoreValue = {
+      overflow: 'hidden',
+      maxHeight: '340px',
+      textOverflow: 'ellipisis'
     }
-  }
-  
-  useEffect(() => {
-    window.addEventListener('scroll', scrollAnaimation);
-    return () => {
-      window.removeEventListener('scroll', scrollAnaimation);
-    };
-  }, []);
+  return !seeMore && seeMoreValue || {}
+}
+const handleDivHeight = () => {
+  const rect = refLang.current?.getBoundingClientRect();
 
-  return (
-    <section className={style.mainContainer}>
-      <div className={style.subContainer}>
-        <figure className={style.containerImg}>
-          <div>
-            <img src="/images/color.jpg" alt="" />
-          </div>
-        </figure>
-        <article className={style.containerText}>
-          <div>
-            <h1 className={style.textTitle}>Sobre mim</h1>
-            <p className={style.subTitle}>Douglas Jansey</p>
-            <p className={style.text}>Desenvolvedor Full Stack</p>
-            <p>
-              {profileDesc}
-            </p>
-          </div>
-        </article>
-      </div>
-      <div ref={refLang}>
+  if (rect) {
+    if (rect.width && rect.height) {
+      // A div está visível na tela, atualiza a altura
+      const divHeight = refLang.current!
+      scrollY > divHeight?.clientHeight && divHeight?.clientHeight > 0 ? setActiveExperience(true) : setActiveExperience(false)
+
+    }
+
+  }
+}
+
+useEffect(() => {
+  window.addEventListener('scroll', scrollAnaimation);
+  return () => {
+    window.removeEventListener('scroll', scrollAnaimation);
+  };
+}, []);
+
+return (
+  <section className={style.mainContainer}>
+    <div className={style.subContainer}>
+      <figure className={style.containerImg}>
+        <div>
+          <img src="/images/color.jpg" alt="" />
+        </div>
+      </figure>
+      <article className={style.containerText}>
+        <div>
+          <h1 className={style.textTitle}>Sobre mim</h1>
+          <p className={style.subTitle}>Douglas Jansey</p>
+          <p className={style.text}>Desenvolvedor Full Stack</p>
+        </div>
+        <div className={style.cardText}>
+          <p style={handleSeeMore()}>
+            {profileDesc}
+          </p>
+          <button onClick={() => setSeeMore(!seeMore)}>ver mais</button>
+        </div>
+      </article>
+    </div>
+    <div ref={refLang} >
         {activeLanguage && languages}
       </div>
-        {activeExperience && experience}
-    </section>
-  );
+      {activeExperience && experience}
+  </section>
+);
 }
